8b8c4552625a082e43595073be0d9ef0fe7b0e12
Shekar B <boggavarpucs@outlook.com>
2025-05-22 10:31:07 -0700

    misc updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-    <h1 className="text-3xl font-bold text-center text-blue-600">HMS-UI</h1>
+    <h1 className="text-3xl font-bold text-center text-blue-600">HMS Remote job & discussion board</h1>
     </>
   );
 }
