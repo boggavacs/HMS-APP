e3393436352356fd979ba306ea9f4f0ca39c42e0
Shekar B <boggavarpucs@outlook.com>
2025-05-30 16:34:56 -0700

    layotu initial changes

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='border-2'>Footer</div>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Header() {
+  return (
+    <div className='flex border-2'>
+      <div className="text-3xl w-1/6 flex flex-col justify-center">
+        <span>HMS </span>
+        <span className="text-tiney">Remote job & discussion board</span>
+      </div>  <div className="w-full">2</div>
+      <div className="w-1/4">3</div>
+    </div>
+  )
+}
